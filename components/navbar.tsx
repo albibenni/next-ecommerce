7c01d1681e1d0d502609bac868d0b063579c1104
Alberto Benatti <albi.benni8@gmail.com>
2023-09-28 11:09:14 +0200

    fixed logo and navbar link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,15 @@
 "use client";
 
 import { Button, Navbar } from "flowbite-react";
+import { BenniLogo } from "./logo";
 
 export default function NavbarWithCTAButton() {
   return (
     <Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img
-          alt="Flowbite React Logo"
-          className="mr-3 h-6 sm:h-9"
-          src="/favicon.svg"
-        />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite React
+      <Navbar.Brand href="https://albertobenatti.com">
+        <BenniLogo className="w-64 lg:w-64" alt="Benni Logo" />
+        <span className="ml-8 self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Next e-Ncommerce
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
