b9d1cbf7ac657fcf1ef9bb2a6316c0e9e9684fc1
Alberto Benatti <albi.benni8@gmail.com>
2023-09-30 13:38:26 +0200

    add prisma generated type

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Product from "@/components/product";
+import { PrismaClient } from "@prisma/client";
+import { Product as ProductType } from "@prisma/client";
 
+const prisma = new PrismaClient();
 export default async function Home() {
-  const res = await fetch("https://fakestoreapi.com/products");
-  const products = await res.json();
+  const products: ProductType[] = await prisma.product.findMany();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Product name={products[0].title} />
+      {products.map((product) => (
+        <Product key={product.id} name={product.name} id={product.id} />
+      ))}
     </main>
   );
 }
