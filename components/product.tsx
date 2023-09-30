7932552151146ff3c713591eda67ec5d1ff4ff80
Alberto Benatti <albi.benni8@gmail.com>
2023-09-30 13:38:36 +0200

    add id for delete api wip

--- components/product.tsx
+++ components/product.tsx
@@ -2,13 +2,18 @@
 
 interface ProductProps {
   name: string;
+  id: number;
 }
 
-export default function Product({ name }: ProductProps) {
+export default function Product({ name, id }: ProductProps) {
+  const deleteProduct = async () => {
+    await fetch(`/api/delete-product/${id}`);
+    console.log("deleted product"); //! remove
+  };
   return (
     <div>
       {name}
-      <button onClick={() => console.log("CLICK")}>CLICK</button>
+      <button onClick={deleteProduct}>CLICK</button>
     </div>
   );
 }
