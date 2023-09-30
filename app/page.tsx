3e26d2636883d1305e7d0535539a84e33ca0e06b
Alberto Benatti <albi.benni8@gmail.com>
2023-09-30 18:11:43 +0200

    page refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,9 @@
 import Product from "@/components/product";
-import { PrismaClient } from "@prisma/client";
-import { Product as ProductType } from "@prisma/client";
-const prisma = new PrismaClient();
-
-export type ProductPrimitive = {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-};
+import { fetchProducts } from "./backend/route";
+import { ProductPrimitive } from "./types";
 
 export default async function Home() {
-  const products: ProductType[] = await prisma.product.findMany();
-  const productMapped: ProductPrimitive[] = products.map((product) => ({
-    id: product.id,
-    name: product.name,
-    description: product.description,
-    price: product.price.toNumber(),
-    image: product.image,
-  }));
+  const productMapped: ProductPrimitive[] = await fetchProducts();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
