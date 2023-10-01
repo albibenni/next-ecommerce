a41ba17c89e35f4562cb7eeb99fb22f30947a1fa
Alberto Benatti <albi.benni8@gmail.com>
2023-10-01 14:34:05 +0200

    wip api with routes

---
+++ app/api/products/route.ts
@@ -0,0 +1,41 @@
+import { ProductPrimitive, toProductPrimitive } from "@/app/types";
+import { PrismaClient, Product as ProductType } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+export const fetchProducts = async (): Promise<ProductPrimitive[]> => {
+  console.log("SERVER: get products");
+  const res: ProductType[] = await prisma.product.findMany();
+  return res.map(toProductPrimitive);
+};
+export const GET = async () => {
+  console.log("SERVER: get products");
+  const products: ProductType[] = await prisma.product.findMany();
+  return NextResponse.json(products.map(toProductPrimitive));
+};
+
+export const DELETE = async (id: string) => {
+  console.log("SERVER: get products");
+  const products: ProductType[] = await prisma.product.findMany();
+  return NextResponse.json(products.map(toProductPrimitive));
+};
+
+export const createProduct = async (
+  product: ProductPrimitive
+): Promise<ProductPrimitive> => {
+  console.log("SERVER: create");
+  const res: ProductType = await prisma.product.create({
+    data: {
+      name: product.name,
+      price: product.price,
+      description: product.description,
+      image: product.image,
+    },
+  });
+  return toProductPrimitive(res);
+};
+
+export const deleteProduct = async (id: number): Promise<void> => {
+  const res = await prisma.product.delete({ where: { id } });
+  console.log(res);
+};
