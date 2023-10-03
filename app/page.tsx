1e8a0a08d77966aa00f0cd87e44748c10889921c
Alberto Benatti <albi.benni8@gmail.com>
2023-10-03 09:50:35 +0200

    page made client comp to re render on delete

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
+"use client";
+
 import Product from "@/components/product";
 import { ProductPrimitive } from "./types";
-import { fetchProducts } from "./api/products/route";
+// import { fetchProducts } from "./api/products/route";
+import { useEffect, useState } from "react";
+import { Product as ProductType } from "@prisma/client";
 
-export default async function Home() {
-  const productMapped: ProductPrimitive[] = await fetchProducts();
+export default function Home() {
+  // const productMapped: ProductPrimitive[] = await fetchProducts();
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const fetchProducts = () => {
+    fetch(`http://localhost:3000/api/products/`, {
+      method: "GET",
+    })
+      .then((r) => r.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const onDelete = () => {
+    fetchProducts();
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {productMapped.map((product) => (
-        <Product key={product.id} product={product} />
+      {products.map((product) => (
+        <Product key={product.id} product={product} onDelete={onDelete} />
       ))}
     </main>
   );
