581e53cd09069e00789daa74566f04034579083f
Alberto Benatti <albi.benni8@gmail.com>
2023-10-03 15:16:41 +0200

    row products

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-row items-center justify-between p-24">
       {products.map((product) => (
         <Product key={product.id} product={product} onDelete={onDelete} />
       ))}
