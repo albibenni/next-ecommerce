acdb458cf79a8ae8937df1d74d7dfa2d8bafdd85
Alberto Benatti <albi.benni8@gmail.com>
2023-10-03 15:41:57 +0200

    handle screen resolution rows and cols

--- prisma/data.ts
+++ prisma/data.ts
@@ -26,6 +26,31 @@ const seedProducts = [
     price: new Prisma.Decimal(12.95),
     image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
   },
+  {
+    name: "Cool helmet.",
+    description: "A nice helmet to wear on your head",
+    price: new Prisma.Decimal(19.95),
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+  },
+  {
+    name: "Grey T-Shirt 2",
+    description: "A nice shirt that you can wear on your body",
+    price: new Prisma.Decimal(22.95),
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  },
+  {
+    name: "Socks 2",
+    description: "Cool socks that you can wear on your feet",
+    price: new Prisma.Decimal(12.95),
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+  },
+  {
+    name: "Sweatshirt 2",
+    description: "Cool sweatshirt that you can wear on your body",
+    price: new Prisma.Decimal(12.95),
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+  },
 ];
 
 export default seedProducts;
