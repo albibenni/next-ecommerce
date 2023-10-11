dd4b85a9638af9ae2e8da981d8380feb1e14b2d6
Alberto Benatti <albi.benni8@gmail.com>
2023-10-11 10:02:21 +0200

    fixed footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,15 @@
+import { redirect } from "next/dist/server/api-utils";
 import { BenniLogo } from "./logo";
 
 export default function Footer() {
   return (
     <footer className="bg-white dark:bg-gray-900">
       <div className="container px-6 py-8 mx-auto">
-        <div className="flex flex-col items-center text-center">
-          <a href=""></a>
-
-          <p className="max-w-md mx-auto mt-4 text-gray-500 dark:text-gray-400">
+        <div className="flex flex-row items-center text-center justify-center">
+          <BenniLogo className="pr-2" width="50" height="50" />
+          <p className="max-w-md mx-auto text-gray-500 dark:text-gray-400">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
-
-          <div className="flex flex-col mt-4 sm:flex-row sm:items-center sm:justify-center">
-            <button className="flex items-center justify-evenly order-1 w-full px-2 py-2 mt-3 text-sm tracking-wide text-gray-600 capitalize transition-colors duration-300 transform border rounded-md sm:mx-2 dark:border-gray-400 dark:text-gray-300 sm:mt-0 sm:w-auto hover:bg-gray-50 focus:outline-none focus:ring dark:hover:bg-gray-800 focus:ring-gray-300 focus:ring-opacity-40">
-              <BenniLogo className="pr-2" width="40" height="40" />
-              <span>Website</span>
-            </button>
-          </div>
         </div>
 
         <hr className="my-10 border-gray-200 dark:border-gray-700" />
@@ -29,7 +22,7 @@ export default function Footer() {
           <div className="flex mt-3 -mx-2 sm:mt-0">
             <a
               href="#"
-              className="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300"
+              className="px-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300"
               aria-label="Reddit"
             >
               {" "}
@@ -38,7 +31,7 @@ export default function Footer() {
 
             <a
               href="#"
-              className="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300"
+              className="px-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300"
               aria-label="Reddit"
             >
               {" "}
@@ -47,7 +40,7 @@ export default function Footer() {
 
             <a
               href="#"
-              className="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300"
+              className="px-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300"
               aria-label="Reddit"
             >
               {" "}
