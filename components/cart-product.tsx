1996e8bad579b8ced01d74f80e6cc7567beca1e6
Alberto Benatti <albi.benni8@gmail.com>
2023-10-11 14:08:52 +0200

    cart product wip

--- components/cart-product.tsx
+++ components/cart-product.tsx
@@ -2,13 +2,14 @@
 
 import { ProductPrimitive } from "@/app/types";
 import { deleteProduct } from "./api";
+import Image from "next/image";
 
-interface ProductProps {
+interface CartProductProps {
   product: ProductPrimitive;
   onDelete: () => void;
 }
 
-export default function Product({ product, onDelete }: ProductProps) {
+export default function CartProduct({ product, onDelete }: CartProductProps) {
   const { image, id, name, description, price } = product;
 
   const handleDelete = async () => {
@@ -17,22 +18,23 @@ export default function Product({ product, onDelete }: ProductProps) {
   };
 
   return (
-    <div className="px-6 py-4 transition-colors duration-200 transform rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700">
-      <p className="text-lg font-medium text-gray-800 dark:text-gray-100">
-        {name}
-      </p>
-      <h4 className="mt-2 text-4xl font-semibold text-gray-800 dark:text-gray-100">
-        ${price}
-      </h4>
-      <p className="mt-4 text-gray-500 dark:text-gray-300">{description}</p>
-      <div className="flex flex-row gap-1">
-        <button
-          className="w-full px-4 py-2 mt-10 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-          onClick={handleDelete}
-        >
-          Delete
-        </button>
-      </div>
+    <div className="grid row-span-1 grid-rows-5 gap-0 justify-center text-center px-6 py-4 transition-colors duration-300 transform rounded-lg hover:bg-gray-300 border-2 border-red-500">
+      <p className="text-lg font-medium text-gray-800 ">{name}</p>
+      <Image
+        className="mx-auto my-auto"
+        src={image}
+        alt={name}
+        width={150}
+        height={150}
+      />
+      <h4 className="text-4xl font-semibold text-gray-800 ">${price}</h4>
+      <p className="text-gray-500 ">{description}</p>
+      <button
+        onClick={handleDelete}
+        className="w-full mt-auto px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+      >
+        Remove
+      </button>
     </div>
   );
 }
